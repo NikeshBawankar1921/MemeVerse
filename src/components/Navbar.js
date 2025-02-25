b24a497d04996635e7e1a35a2b15437be55a3324
nikesh bawankar <nikeshbawankar1921@gmail.com>
2025-02-25 16:31:17 +0530

    re commit navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
   return (
     <>
       {/* Fixed Header */}
-      <nav className="fixed top-0 left-0 w-full  dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 z-50">
+      <nav className="fixed top-0 left-0 w-full  bg-gray-900 border-b border-gray-200 dark:border-gray-700 z-50">
         <div className="max-w-screen-xl mx-auto px-4">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center text-white">
