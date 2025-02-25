b56b7c47dac7e9653f369fd8de1ff6767f8874ef
nikesh bawankar <nikeshbawankar1921@gmail.com>
2025-02-25 17:15:38 +0530

    ...

--- src/pages/Explorer.js
+++ src/pages/Explorer.js
@@ -146,7 +146,7 @@ const Explorer = () => {
 
       {/* Memes Grid */}
       <motion.div 
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-white"
         layout
       >
         {displayedMemes.map((meme) => (
